1940ea638a0c73e1f1a3177ce7a5f9d74f82a364
Federico Berlfein <berlfein@m005.opa.vera.psc.edu>
2025-04-08 15:12:38 -0400

    Small change to out log

--- chromatic/run_coad.sh
+++ chromatic/run_coad.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task 28
 #SBATCH --time 48:00:00
 #SBATCH --job-name coadd
-#SBATCH -o ./output/logs/out.log
-#SBATCH -e ./output/logs/out.log
+#SBATCH -o ./output/log_files/out.txt
+#SBATCH -e ./output/log_files/out.txt
 
 cd $SLURM_SUBMIT_DIR
 python run_coadd.py
